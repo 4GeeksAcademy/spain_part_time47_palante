5b6d2e46c2d7dfbea62a6d8ac21d01f13e1d4210
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-10-29 18:57:52 +0000

    Modificacion en la tablas

--- src/api/models.py
+++ src/api/models.py
@@ -9,7 +9,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), unique=False, nullable=False)
     last_name = db.Column(db.String(60), unique=False, nullable=False)
-    user_name = db.Column(db.String(40), unique=True, nullable=True)
     email = db.Column(db.String(60), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -22,17 +21,15 @@ class User(db.Model):
             "id": self.id,
             "name": self.name,
             "last_name": self.last_name,
-            "user_name": self.user_name,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
     
-class Admin(db.Model):
-    __tablename__ = 'admin'
+class Admini(db.Model):
+    __tablename__ = 'admini'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), unique=False, nullable=False)
     last_name = db.Column(db.String(60), unique=False, nullable=False)
-    user_name = db.Column(db.String(40), unique=True, nullable=True)
     phone_number = db.Column(db.Integer, unique=True, nullable=False)
     email = db.Column(db.String(60), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -40,14 +37,13 @@ class Admin(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<Admin %r>' % self.id
+        return '<Admini %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
             "name": self.name,
             "last_name": self.last_name,
-            "user_name": self.user_name,
             "phone_number": self.phone_number,
             "email": self.email,
             # do not serialize the password, its a security breach
@@ -58,13 +54,14 @@ class Freelancer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), unique=False, nullable=False)
     last_name = db.Column(db.String(60), unique=False, nullable=False)
-    user_name = db.Column(db.String(40), unique=True, nullable=True)
-    phone_number = db.Column(db.Integer, unique=True, nullable=False)
+    age = db.Column(db.Integer, unique=False, nullable=False)
     email = db.Column(db.String(60), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    registration_date = db.Column(DateTime, default=datetime.utcnow)
-    hourly_rate = db.Column(db.Integer, unique=False, nullable=True)
-    available = db.Column(db.Boolean(), unique=False, nullable=False)
+    registration_date = db.Column(db.DateTime, default=datetime.utcnow)
+    professional_registration_number = db.Column(db.Integer, unique=True)
+    years_of_experience = db.Column(db.Integer, unique=False)
+    education = db.Column(db.String(120), unique=False)
+    expertise = db.Column(db.String(200), unique=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -75,11 +72,12 @@ class Freelancer(db.Model):
             "id": self.id,
             "name": self.name,
             "last_name": self.last_name,
-            "user_name": self.user_name,
-            "phone_number": self.phone_number,
+            "age": self.age,
             "email": self.email,
-            "hourly_rate": self.hourly_rate,
-            "available": self.available,
+            "professional_registration_number": self.professional_registration_number,
+            "years_of_experience": self.years_of_experience,
+            "education": self.education,
+            "expertise": self.expertise,
             # do not serialize the password, its a security breach
         }
     
@@ -88,11 +86,11 @@ class Volunteer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), nullable=False)
     last_name = db.Column(db.String(60), nullable=False)
-    user_name = db.Column(db.String(40), unique=True, nullable=True)
     phone_number = db.Column(db.Integer, unique=True, nullable=False)
     email = db.Column(db.String(60), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
     registration_date = db.Column(db.DateTime, default=datetime.utcnow)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return '<Volunteer %r>' % self.id
@@ -102,51 +100,7 @@ class Volunteer(db.Model):
             "id": self.id,
             "name": self.name,
             "last_name": self.last_name,
-            "user_name": self.user_name,
             "phone_number": self.phone_number,
             "email": self.email,
             # do not serialize the password, it's a security breach
         }
-
-    
-class Skills(db.Model):
-    __tablename__ = 'skills'
-    id = db.Column(db.Integer, primary_key=True)
-    skill = db.Column(db.String(80), nullable=False)
-    description = db.Column(db.String(100), nullable=True)
-    proficiency = db.Column(db.String(20), nullable=True)
-    experience_years = db.Column(db.Integer, nullable=True)
-    freelancer_id = db.Column(db.Integer, db.ForeignKey(Freelancer.id))
-    freelancer = db.relationship(Freelancer, back_populates="skills")
-    volunteer_id = db.Column(db.Integer, db.Foreignkey(Volunteer.id))
-    volunteer = db.relationship(Volunteer, back_populates="skills ")
-    def __repr__(self):
-        return '<Skills %r>' % self.id
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "skill": self.skill,
-            "description": self.description,
-            "proficiency": self.proficiency,
-            "experience_years": self.experience_years,
-        }    
-
-class Project(db.Model):
-    __tablename__ = 'project'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    description = db.Column(db.String(250), nullable=False)
-    freelancer_id = db.Column(db.Integer, db.ForeignKey(Freelancer.id))
-    freelancer = db.relationship(Freelancer, back_populates="project")    
-
-    def __repr__(self):
-        return '<Project %r>' % self.id
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "description": self.description,
-        }
-    
