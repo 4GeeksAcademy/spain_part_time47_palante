8935cafb917e9ceab06677a53477b9a25621b555
NelysT <139475672+NelysT@users.noreply.github.com>
2023-10-30 14:43:57 +0000

    navbar - pongo los principales vistas

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,18 +1,73 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
+	const navbarStyle = {
+		position: "fixed",
+		top: 0,
+		left: 0,
+		width: "100%",
+		zIndex: 100
+		
+		
+	  };
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-expand-lg bg-body-tertiary" style={navbarStyle}>
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<p>P'ALANTE</p>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+				
+				<div className="container_navbar_right d-flex">
+					<div className="dropdown">
+						<button className="btn_comunidad dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Comunidad
+						</button>
+						<ul className="dropdown-menu">
+							<li><a className="dropdown-item" href="#">Chat</a></li>
+							<li><a className="dropdown-item" href="#">Quedadas</a></li>
+							
+						</ul>
+					</div>
+					<div className="dropdown">
+						<button className="btn_recursos dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Tools
+						</button>
+						<ul className="dropdown-menu">
+							<Link to='/lecturas'>
+								<li><a className="dropdown-item" href="#">Lecturas</a></li>
+							</Link>
+							<li><a className="dropdown-item" href="#">Meditaciones guiadas</a></li>
+							<li><a className="dropdown-item" href="#">Entrevistas</a></li>
+							<Link to='/podcast'>
+							<li><a className="dropdown-item" href="#">Podcast</a></li>
+							</Link>
+							
+							<li><a className="dropdown-item" href="#">De tus Freelance</a></li>
+						</ul>
+					</div>
+					<div className="dropdown">
+						<button className="btn_citas dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Citas
+						</button>
+						<ul className="dropdown-menu">
+							<li><a className="dropdown-item" href="#">Profesionales</a></li>
+							<li><a className="dropdown-item" href="#">Voluntarios</a></li>
+							
+						</ul>
+					</div>
+					<div className="login">
+						<Link to='/login'>
+							<button className="btn_login" type="button">
+							Login
+							</button>
+						</Link>
+						
+					</div>
+					
+				</div>	
+				
 			</div>
 		</nav>
 	);
