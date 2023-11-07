1a152d1844c46ee6cb8f178610679b09d637ebd0
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-07 17:05:18 +0000

    Modificacion en las rutas

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,13 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
-import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
+import { Login } from "./vistas/login";
+import { Signup_user } from "./vistas/signup-user";
+import { Signup_freelancer } from "./vistas/signup-freelance";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+
 
 //create your first component
 const Layout = () => {
@@ -25,9 +26,9 @@ const Layout = () => {
                 <ScrollToTop>
                     <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<Login />} path="/" />
+                        <Route element={<Signup_user />} path="/signup-user" />
+                        <Route element={<Signup_freelancer />} path="/signup-freelance" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
