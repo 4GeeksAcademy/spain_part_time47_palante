b27c416ed8199d0a5462cb4a01a8a27be501d55c
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-07 17:47:47 +0000

    Componente signup-user con estructura basica y guardando datos

--- src/front/js/vistas/signup-user.js
+++ src/front/js/vistas/signup-user.js
@@ -4,38 +4,23 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const Signup_user = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
+  const [register, setRegister] = useState({full_name:'', email:'', password:''})
 
-	return (
-		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
-					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
-					);
-				})}
-			</ul>
-			<br />
-			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
-			</Link>
-		</div>
-	);
+  return (
+    <div className="container">
+      <h1>Registro de usuario</h1>
+
+      <label for="exampleInputEmail1" class="form-label">Full name *</label>
+      <input type="name" class="form-control" name="full name" id="exampleInputEmail1" aria-describedby="emailHelp" value={register.full_name} onChange={(e)=>setRegister(e.target.value)} />
+
+      <label for="exampleInputEmail1" class="form-label">Email *</label>
+      <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" value={register.email} onChange={(e)=>setRegister(e.target.value)} />
+
+      <label for="exampleInputEmail1" class="form-label">Password *</label>
+      <input type="Password" class="form-control" name="Password" id="exampleInputEmail1" aria-describedby="emailHelp" value={register.password} onChange={(e)=>setRegister(e.target.value)} />
+      <br/>
+      <button>submit</button>
+    </div>
+  );
 };
