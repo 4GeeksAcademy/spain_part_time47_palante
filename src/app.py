63036b415a2c08a951d2cfceafb8c8da6d5e7ecd
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-07 12:18:09 +0000

    Endpoints de usuario estan funcionando

--- src/app.py
+++ src/app.py
@@ -7,17 +7,29 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db, User, Freelancer
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
+from flask_bcrypt import Bcrypt
 #from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = os.environ.get("SUPER_SECRET")  # Change this!
+jwt = JWTManager(app)
+
+bcrypt = Bcrypt(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -63,6 +75,52 @@ def serve_any_other_file(path):
     response.cache_control.max_age = 0 # avoid cache memory
     return response
 
+                   ######### USUARIOS #########
+
+##### ruta de registro de usuario #####
+@app.route("/user-register", methods=['POST'])
+def user_register():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify('body must be sent'), 400
+    if 'full_name' not in body:
+        return jsonify('full_name is required'), 400
+    if 'email' not in body:
+        return jsonify('email is required'), 400
+    if 'password' not in body:
+        return jsonify('password is required'), 400 
+       
+    pw_hash = bcrypt.generate_password_hash(body['password']).decode('utf-8')
+    new_user = User(full_name = body['full_name'], email = body['email'], password = pw_hash, is_active = True)
+    
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify('Successful registration'),200
+
+##### ruta de inicio de sesion de usuario #####
+@app.route("/user-login", methods=["POST"])
+def user_login():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify('body must be sent'), 400
+    if 'email' not in body:
+        return jsonify('email is required'), 400
+    if 'password' not in body:
+        return jsonify('password is required'), 400
+
+    user = User.query.filter_by(email=body['email']).first()
+    if user is None or user.email != body['email'] or not bcrypt.check_password_hash(user.password, body['password']):#user.password != body['password']: 
+        return jsonify('incorrect email or password'),400
+
+    access_token = create_access_token(identity=body['email'])
+    return jsonify(access_token=access_token)
+
+##### ruta privada de usuario #####
+@app.route("/user-private", methods=['GET'])
+@jwt_required()
+def user_private():
+    email = get_jwt_identity()
+    return jsonify(email = email)
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
