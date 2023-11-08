1e1f3bacd1a054e5882d62eba7dea84981bf05cf
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-08 10:40:50 +0000

    El fetch funciona correctamente

--- src/front/js/vistas/signup-user.js
+++ src/front/js/vistas/signup-user.js
@@ -1,26 +1,56 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
 
 import { Context } from "../store/appContext";
 
 export const Signup_user = () => {
   const { store, actions } = useContext(Context);
-  const [register, setRegister] = useState({full_name:'', email:'', password:''})
+  const [register, setRegister] = useState({ full_name: '', email: '', password: '' })
+  const [error, setError] = useState('')
+
+  //La función handleSubmit se encarga de tomar los datos del formulario de registro y los envia al servidor en formato JSON.
+  const handleSubmit = (e) => {
+    e.preventDefault(); //Previene el comportamiento por defecto del navegador
+  
+    //Verifica que los campos "email" y "password" esten completos 
+  if (register.full_name.trim() === '' || register.email.trim() === '' || register.password.trim() === '') {      //##### Debo ver como mostras el mensaje al campo vacio 
+    setError('All fields are mandatory!');
+    return;
+  }
+  //Envio los datos del formulario a una tabla a traves de un fetch con metodo "post"  
+  fetch(process.env.BACKEND_URL + "/user-register", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(register),
+  })
+  .then(resp => resp.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error))
+
+  //Limpia los campos una vez creado el usuario exitosamente, da paso a poder seguir creando registros
+  setRegister({
+   full_name:'', email: '', password: ""
+  });
+}
 
   return (
     <div className="container">
-      <h1>Registro de usuario</h1>
+      <form className="from" onSubmit={handleSubmit}>
+        <h1>Registro de usuario</h1>
 
-      <label for="exampleInputEmail1" class="form-label">Full name *</label>
-      <input type="name" class="form-control" name="full name" id="exampleInputEmail1" aria-describedby="emailHelp" value={register.full_name} onChange={(e)=>setRegister(e.target.value)} />
+        <label for="exampleInputEmail1" class="form-label">Full name *</label>
+        <input type="name" className="form-control" name="full name" id="exampleInputEmail1" aria-describedby="emailHelp" value={register.full_name} onChange={(e) => setRegister({...register,full_name:e.target.value})} />
+  
+        <label for="exampleInputEmail1" class="form-label">Email *</label>
+        <input type="email" className="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" value={register.email} onChange={(e) => setRegister({...register,email:e.target.value})} />
 
-      <label for="exampleInputEmail1" class="form-label">Email *</label>
-      <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" value={register.email} onChange={(e)=>setRegister(e.target.value)} />
-
-      <label for="exampleInputEmail1" class="form-label">Password *</label>
-      <input type="Password" class="form-control" name="Password" id="exampleInputEmail1" aria-describedby="emailHelp" value={register.password} onChange={(e)=>setRegister(e.target.value)} />
-      <br/>
-      <button>submit</button>
+        <label for="exampleInputEmail1" class="form-label">Password *</label>
+        <input type="Password" className="form-control" name="Password" id="exampleInputEmail1" aria-describedby="emailHelp" value={register.password} onChange={(e) => setRegister({...register,password:e.target.value})} />
+        <br />
+        <button>submit</button>
+      </form>
     </div>
   );
-};
+  
+}
